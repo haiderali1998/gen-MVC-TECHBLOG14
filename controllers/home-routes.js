5cc7da7157655e30a9909d1ed3ae6d98c615faa0
haiderali9823-xSomaK <haiderali9823@outlook.com>
2022-10-08 03:11:46 +0100

    push routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,41 +3,22 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require("./../models");
 
 router.get('/', (req, res) => {
-  Post.findAll({
-    attributes: [
-      'id',
-      'title',
-      "post_info",
-      'created_at'      
-    ],
-    include: [
-      {
-        model: Comment,
-        attributes: ['id', 'comment_info', 'post_id', 'user_id', 'created_at'],
-        include: {
-          model: User,
-          attributes: ['username']
-        }
-      },
-      {
-        model: User,
-        attributes: ['username']
-      }
-    ]
-  })
-    .then(dbPostData => {
-      const posts = dbPostData.map(post => post.get({ plain: true }));
-      // pass a single post object into the homepage template
-      res.render('homepage', { 
-        posts,
-        loggedIn: req.session.loggedIn 
-      });
+    Post.findAll({
+      include: [User, Comment],
     })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
+      .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        // pass a single post object into the homepage template
+        res.render('homepage', { 
+          posts,
+          loggedIn: req.session.loggedIn 
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
