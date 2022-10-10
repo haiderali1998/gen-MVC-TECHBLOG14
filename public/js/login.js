556d9c7c592cfb862fc16cbf2ec80317e47a35eb
haiderali9823-xSomaK <haiderali9823@outlook.com>
2022-10-10 02:25:47 +0100

    push heroku

--- public/js/login.js
+++ public/js/login.js
@@ -2,7 +2,7 @@ const loginFormHandler = async (event) => {
   event.preventDefault();
 
  
-  //const email = document.querySelector('#email-login').value.trim();
+  const username = document.querySelector('#username-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
   if (username && password) {
@@ -27,14 +27,14 @@ const loginFormHandler = async (event) => {
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#name-signup').value.trim();
+  const username = document.querySelector('#username-signup').value.trim();
   const email = document.querySelector('#email-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
 
-  if (name && email && password) {
+  if (username && email && password) {
     const response = await fetch('/api/users', {
       method: 'POST',
-      body: JSON.stringify({ name, email, password }),
+      body: JSON.stringify({ username, email, password }),
       headers: { 'Content-Type': 'application/json' },
     });
 
