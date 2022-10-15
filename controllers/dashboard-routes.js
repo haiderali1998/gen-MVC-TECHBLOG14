536e4455cd52eb95f3dc2c15a8201ba75300ebff
haiderali1998 <haiderali9823@outlook.com>
2022-10-15 15:14:36 +0100

    create post

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -2,24 +2,45 @@ const router = require('express').Router();
 const { Post } = require('../models/');
 const withAuth = require('../utils/auth');
 
+// router.get('/', withAuth, async (req, res) => {
+//   try {
+//     const postData = await Post.findAll({
+//       where: {
+//         userId: req.session.userId,
+//       },
+//     });
+
+//     const posts = postData.map((post) => post.get({ plain: true }));
+
+//     res.render('all-posts-admin', {
+//       layout: 'dashboard',
+//       posts,
+//     });
+//   } catch (err) {
+//     res.redirect('login');
+//   }
+// });
+
 router.get('/', withAuth, async (req, res) => {
-  try {
-    const postData = await Post.findAll({
-      where: {
-        userId: req.session.userId,
-      },
-    });
-
-    const posts = postData.map((post) => post.get({ plain: true }));
-
-    res.render('all-posts-admin', {
-      layout: 'dashboard',
-      posts,
-    });
-  } catch (err) {
-    res.redirect('login');
-  }
-});
+    try {
+      const postData = await Post.findAll({
+        where: {
+          id: req.session.user_id,  // ==> should be id: req.session.user_id,
+        },
+      });
+  
+      const posts = postData.map((post) => post.get({ plain: true }));
+  
+      // you don't have a template called all-posts-admin, so this will fail
+      // you could choose to just render the dashboard like: res.render('dashboard', {posts})
+      res.render('create-post', {
+        layout: 'dashboard',
+        posts,
+      });
+    } catch (err) {
+      res.redirect('login');
+    }
+  });
 
 router.get('/new', withAuth, (req, res) => {
   res.render('new-post', {
@@ -45,5 +66,6 @@ router.get('/edit/:id', withAuth, async (req, res) => {
     res.redirect('login');
   }
 });
+
 
 module.exports = router;
